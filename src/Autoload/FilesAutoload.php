940d8ef5ebc9be6b52cf51b1878c6b99a22ac669
wheakerd <wheakerd@gmail.com>
2026-01-14 13:20:17 +0800

    feat: cached.

---
+++ src/Autoload/FilesAutoload.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace SuperKernel\Composer\Autoload;
+
+use SuperKernel\Composer\Constant\AutoloadType;
+use SuperKernel\Composer\Contract\AutoloadInterface;
+
+final class FilesAutoload implements AutoloadInterface
+{
+	public function type(): AutoloadType
+	{
+		// TODO: Implement type() method.
+	}
+
+	public function metadata(): array
+	{
+		// TODO: Implement metadata() method.
+	}
+}
